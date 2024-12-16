2a9d0c92dc7ee81d04a09edd872f030dac72fc97
Owens-D10 <Owens-D10@ulster.ac.uk>
2024-12-16 08:42:16 +0000

    Cleaned a few folders and scripts

--- Assets/Wolfmother/Scripts/Camera/CameraSwitch.cs
+++ Assets/Wolfmother/Scripts/Camera/CameraSwitch.cs
@@ -7,12 +7,11 @@ public class CameraSwitch : MonoBehaviour
 {
     public GameObject From;
     public GameObject To;
-    public bool camOn = false;
-    public int cameraNumber;
+    
     
     void Start()
     {
-        cameraNumber = 1;
+        
     }
 
     // Update is called once per frame
